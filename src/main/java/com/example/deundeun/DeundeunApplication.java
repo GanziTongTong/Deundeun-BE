8df42d0c55ed5f5f72c941b1c91b883f18eef88a
ckr8305 <ckr8305@naver.com>
2025-11-10 18:11:55 +0900

    Feat: 프로젝트 초기설정

---
+++ src/main/java/com/example/deundeun/DeundeunApplication.java
@@ -0,0 +1,13 @@
+package com.example.deundeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeundeunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeundeunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deundeun/DeundeunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deundeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeundeunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
